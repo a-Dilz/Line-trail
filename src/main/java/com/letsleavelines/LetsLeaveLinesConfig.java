939c88ea57db7dda520c00b28a4e96f1e0c058bf
a-Dilz <dillon.moore92@gmail.com>
2024-12-30 21:48:23 -0400

    ?

--- src/main/java/com/letsleavelines/LetsLeaveLinesConfig.java
+++ src/main/java/com/letsleavelines/LetsLeaveLinesConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.letsleavelines;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface LetsLeaveLinesConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/main/java/com/letsleavelines/LetsLeaveLinesPlugin.java
+++ src/main/java/com/letsleavelines/LetsLeaveLinesPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.letsleavelines;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,17 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+		name = "LetsLeaveLines",
+		description = "A potentially fun game?",
+		tags = {"game", "lines", "friends", "trail", "event"}
 )
-public class ExamplePlugin extends Plugin
+public class LetsLeaveLinesPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private LetsLeaveLinesConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +48,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    LetsLeaveLinesConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(LetsLeaveLinesConfig.class);
 	}
 }
